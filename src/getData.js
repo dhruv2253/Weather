ca831a3c530ecd806639ba2eda74462aa90590fe
Dhruv Patel <dhruv225@outlook.com>
2023-03-07 17:32:00 -0500

    start connecting html text to api information

--- src/getData.js
+++ src/getData.js
@@ -2,7 +2,7 @@ const key = 'd3f06a0f8adbf844ad69d2ba2196288c';
 
 async function getData(city) {
     try {
-        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${key}`,{mode: 'cors'})
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${key}&units=imperial`,{mode: 'cors'})
         console.log('hi');
         const data = await response.json();
         return data;
--- src/index.js
+++ src/index.js
@@ -2,9 +2,14 @@ import {getData} from "./getData";
 
 const inputField = document.querySelector('input');
 const searchButton = document.querySelector('button');
-function displayInfo() {
-    let input = inputField.value;
-    console.log(getData(input))
+const cityName = document.querySelector('.city-name');
+const temp = document.querySelector('.temp');
+async function displayInfo() {
+    const city = inputField.value;
+    const fetchedData = await getData(city)
+    cityName.textContent = fetchedData.name;
+    temp.textContent = fetchedData.main.temp
+    console.log(fetchedData)
     
  }
 
