e117119d334b3681cd54f7e3d6b91b621769b102
Dhruv Patel <dhruv225@outlook.com>
2023-03-07 22:56:07 -0500

    display to screen method

--- src/index.js
+++ src/index.js
@@ -4,11 +4,20 @@ const inputField = document.querySelector('input');
 const searchButton = document.querySelector('button');
 const cityName = document.querySelector('.city-name');
 const temp = document.querySelector('.temp');
+const feelsLike = document.querySelector('.feels');
+const weatherDescription = document.querySelector('.description');
+const windSpeed = document.querySelector('.wind-speed');
+const time = document.querySelector('.time');
+
 async function displayInfo() {
     const city = inputField.value;
     const fetchedData = await getData(city)
     cityName.textContent = fetchedData.name;
-    temp.textContent = fetchedData.main.temp
+    temp.textContent = "Temperature: " + fetchedData.main.temp
+    feelsLike.textContent = "Feels Like: " + fetchedData.main.feels_like;
+    weatherDescription.textContent = "Description: " + fetchedData.weather[0].description;
+    windSpeed.textContent = "Wind speed: " + fetchedData.wind.speed + "mph";
+   
     console.log(fetchedData)
     
  }
