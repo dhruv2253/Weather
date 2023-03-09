2e9e39f5ae7e6f55105bd5f0aa3116baed9c6455
Dhruv Patel <dhruv225@outlook.com>
2023-03-09 00:23:59 -0500

    added unit conversion button and default city

--- src/getData.js
+++ src/getData.js
@@ -1,9 +1,8 @@
 const key = 'd3f06a0f8adbf844ad69d2ba2196288c';
-
-async function getData(city) {
+async function getData(city, unit) {
     try {
-        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${key}&units=imperial`,{mode: 'cors'})
-        console.log('hi');
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${key}&units=${unit}`,{mode: 'cors'})
+        
         const data = await response.json();
         return data;
     } catch(e) {
--- src/index.js
+++ src/index.js
@@ -7,19 +7,84 @@ const temp = document.querySelector('.temp');
 const feelsLike = document.querySelector('.feels');
 const weatherDescription = document.querySelector('.description');
 const windSpeed = document.querySelector('.wind-speed');
-const time = document.querySelector('.time');
+const errorMessage = document.querySelector('.message');
+const unitsButton = document.querySelector('.units');
+// TODO: add farenheit and celsius conversion. and add symbols to temperature. add pictures for different weathers
+let farenheitUnit = true;
+unitsButton.textContent = 'Display C';
+async function loadDefault() {
+    inputField.value = 'Atlanta';
+    const defaultCityData = await getData('Atlanta', 'imperial')
+        cityName.textContent = defaultCityData.name;
+        temp.textContent = "Temperature: " + Math.ceil(defaultCityData.main.temp)
+        feelsLike.textContent = "Feels Like: " + Math.ceil(defaultCityData.main.feels_like);
+        weatherDescription.textContent = "Description: " + defaultCityData.weather[0].description;
+        windSpeed.textContent = "Wind speed: " + Math.ceil(defaultCityData.wind.speed) + " mph";
+    
+}
+unitsButton.addEventListener('click', switchUnit);
 
-async function displayInfo() {
+async function switchUnit() {
+    if (farenheitUnit) {
+        let unit = 'metric'
+        displayInfo(unit);
+        farenheitUnit = false;
+        unitsButton.textContent = 'display F'
+    }
+    else {
+        let unit = 'imperial';
+        displayInfo(unit);
+        farenheitUnit = true;
+        unitsButton.textContent = 'display C'
+    }
+}
+
+ async function displayInfo(unit) {
+    
+    unitsButton.textContent = 'Display C'
+    try {
     const city = inputField.value;
-    const fetchedData = await getData(city)
-    cityName.textContent = fetchedData.name;
-    temp.textContent = "Temperature: " + fetchedData.main.temp
-    feelsLike.textContent = "Feels Like: " + fetchedData.main.feels_like;
-    weatherDescription.textContent = "Description: " + fetchedData.weather[0].description;
-    windSpeed.textContent = "Wind speed: " + fetchedData.wind.speed + "mph";
-   
-    console.log(fetchedData)
+    console.log(farenheitUnit);
+    if (farenheitUnit){ 
+       
+        const fetchedData = await getData(city, unit)
+        cityName.textContent = fetchedData.name;
+        temp.textContent = "Temperature: " + Math.ceil(fetchedData.main.temp)
+        feelsLike.textContent = "Feels Like: " + Math.ceil(fetchedData.main.feels_like);
+        weatherDescription.textContent = "Description: " + fetchedData.weather[0].description;
+        windSpeed.textContent = "Wind speed: " + Math.ceil(fetchedData.wind.speed) + " mph";
+        errorMessage.textContent = '';
+        console.log(fetchedData)
+    } else {
+        const fetchedData = await getData(city, unit);
+        cityName.textContent = fetchedData.name;
+        temp.textContent = "Temperature: " + Math.ceil(fetchedData.main.temp)
+        feelsLike.textContent = "Feels Like: " + Math.ceil(fetchedData.main.feels_like);
+        weatherDescription.textContent = "Description: " + fetchedData.weather[0].description;
+        windSpeed.textContent = "Wind speed: " + Math.ceil(fetchedData.wind.speed) + " kph";
+        errorMessage.textContent = '';
+        console.log(fetchedData)
+    }
+    
+    } catch(e) {
+        cityName.textContent = 'City Unavailable';
+        errorMessage.textContent = "Enter a valid city";
+        
+    }
+
+
+
     
  }
 
- searchButton.addEventListener('click', displayInfo)
+ async function switchDisplay() {
+    
+ }
+
+
+loadDefault();
+ searchButton.addEventListener('click', () =>{
+    farenheitUnit = true;
+    const unit = 'imperial';
+    displayInfo(unit);
+ });
